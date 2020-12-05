8a0b74b51e97fcc145d7cc5f0167a0441ae00bf7
Gustavo L. de M. Chaves <gustavo@cpqd.com.br>
2020-12-05 16:15:58 -0300

    05 - yet more improvements
    Now both problems have single-line solutions. ;)

--- 2020/05/my-seat-id.pl
+++ 2020/05/my-seat-id.pl
@@ -1,14 +1,6 @@
 #!/usr/bin/env perl
 
 use 5.030;
-use warnings;
 use List::Util qw(reduce);
 
-my $seat;
-
-reduce {$seat = $a+1 if $b - $a == 2; $b}
-    sort {$a <=> $b}
-    map {tr/FBLR/0101/; eval "0b$_"}
-    <>;
-
-say $seat;
+reduce {say $a+1 if $b - $a == 2; $b} sort {$a <=> $b} map {tr/FBLR/0101/; eval "0b$_"} <>;
