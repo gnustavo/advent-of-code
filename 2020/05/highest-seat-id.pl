19a642fd5a59d5e5609f36aa79659527ed790914
Gustavo L. de M. Chaves <gustavo@cpqd.com.br>
2020-12-05 09:39:37 -0300

    05 - improvements
    After seeing my son's solutions I learned of some things to improve on
    the solutions.

--- 2020/05/highest-seat-id.pl
+++ 2020/05/highest-seat-id.pl
@@ -1,20 +1,6 @@
 #!/usr/bin/env perl
 
 use 5.030;
-use autodie;
-use warnings;
+use List::Util qw(max);
 
-my $highest_seat_id = -1;
-
-foreach (<>) {
-    tr/FBLR/0101/;
-
-    my $row = eval '0b' . substr($_, 0, 7);
-    my $col = eval '0b' . substr($_, 7, 3);
-
-    my $seat_id = $row * 8 + $col;
-
-    $highest_seat_id = $seat_id if $highest_seat_id < $seat_id;
-}
-
-say $highest_seat_id;
+say max map {tr/FBLR/0101/; eval "0b$_"} <>;
--- 2020/05/my-seat-id.pl
+++ 2020/05/my-seat-id.pl
@@ -4,24 +4,13 @@ use 5.030;
 use autodie;
 use warnings;
 
-my %seats;
+my @seats = sort {$a <=> $b} map {tr/FBLR/0101/; eval "0b$_"} <>;
 
-foreach (<>) {
-    tr/FBLR/0101/;
-
-    my $row = eval '0b' . substr($_, 0, 7);
-    my $col = eval '0b' . substr($_, 7, 3);
-
-    my $seat = $row * 8 + $col;
-
-    $seats{$seat} = undef;
-}
-
-my @seats = sort {$a <=> $b} keys %seats;
-
-for my $seat ($seats[0]+1 .. $seats[-1]-1) {
-    if (! exists $seats{$seat} && exists $seats{$seat-1} && exists $seats{$seat+1}) {
-        say $seat;
-        last;
+for (my $i=0; $i < $#seats; ++$i) {
+    if ($seats[$i+1] - $seats[$i] == 2) {
+        say $seats[$i] + 1;
+        exit 0;
     }
 }
+
+die "No seat found!\n";
