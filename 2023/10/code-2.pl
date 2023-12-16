482be4d2c5b6d330548592ed03d34eceae1b2f6f
Gustavo L. de M. Chaves <gustavo@cpqd.com.br>
2023-12-16 13:32:02 -0300

    AOC 2023 10 2

--- 2023/10/code-2.pl
+++ 2023/10/code-2.pl
@@ -82,23 +82,117 @@ while (my ($y, $row) = each @grid) {
     }
 }
 
-# Find one initial valid direction
-my $dir;
+# Find valid initial directions
+my %directions = (
+    N => [0, -1],
+    L => [+1, 0],
+    S => [0, +1],
+    W => [-1, 0],
+);
+my @directions;
 DIRECTION:
-for my $d ([-1, 0], [0, +1], [+1, 0], [0, -1]) {
+foreach my $name (qw/N L S W/) {
+    my $d = $directions{$name};
     if ($pipes{$grid[$S->[1]+$d->[1]][$S->[0]+$d->[0]]}->([$S->[1]+$d->[1], $S->[0]+$d->[0]], [$d->[0], $d->[1]])) {
-        $dir = [$d->[0], $d->[1]];
-        last DIRECTION;
+        push @directions, [$name, $d];
     }
 }
 
+# Select initial direction
+my $dir = $directions[0][1];
+
+# Select initial position
 my $pos = [$S->[0]+$dir->[0], $S->[1]+$dir->[1]];
 
-my $cycle_length = 1;
-
+# Mark the cycle in a new grid
+my @cycle_grid = map {[('.') x $grid[0]->@*]} 0 .. $#grid;
 while ($pos->[0] != $S->[0] || $pos->[1] != $S->[1]) {
-    ($pos, $dir) = $pipes{$grid[$pos->[1]][$pos->[0]]}->($pos, $dir);
-    $cycle_length += 1;
+    my $char = $grid[$pos->[1]][$pos->[0]];
+    $cycle_grid[$pos->[1]][$pos->[0]] = $char;
+    ($pos, $dir) = $pipes{$char}->($pos, $dir);
 }
 
-say $cycle_length / 2;
+# Mark the start position
+$cycle_grid[$S->[1]][$S->[0]] = {
+    N => {
+        L => 'L',
+        S => '|',
+        W => 'J',
+    },
+    L => {
+        S => 'F',
+        W => '-',
+    },
+    S => {
+        W => '7',
+    },
+}->{$directions[0][0]}{$directions[1][0]};
+#say "S($S->[1], $S->[0])='$cycle_grid[$S->[1]][$S->[0]]'";
+
+# Calculate the area using the ray casting algorithm
+
+# A state-machine mapping (status, char) => (area, next-status)
+my %algorithm = (
+    'outside' => {
+        '.' => [0, 'outside'],
+        '|' => [0, 'entering'],
+        'L' => [0, 'outside-L'],
+        'F' => [0, 'outside-F'],
+    },
+    'inside' => {
+        '.' => [1, 'inside'],
+        '|' => [0, 'leaving'],
+        'L' => [0, 'inside-L'],
+        'F' => [0, 'inside-F'],
+    },
+    'entering' => {
+        '.' => [1, 'inside'],
+        '|' => [0, 'leaving'],
+        'L' => [0, 'inside-L'],
+        'F' => [0, 'inside-F'],
+    },
+    'leaving' => {
+        '.' => [0, 'outside'],
+        '|' => [0, 'entering'],
+        'L' => [0, 'outside-L'],
+        'F' => [0, 'outside-F'],
+    },
+    'outside-L' => {
+        '-' => [0, 'outside-L'],
+        'J' => [0, 'leaving'],
+        '7' => [0, 'entering'],
+    },
+    'outside-F' => {
+        '-' => [0, 'outside-F'],
+        'J' => [0, 'entering'],
+        '7' => [0, 'leaving'],
+    },
+    'inside-L' => {
+        '-' => [0, 'inside-L'],
+        'J' => [0, 'entering'],
+        '7' => [0, 'leaving'],
+    },
+    'inside-F' => {
+        '-' => [0, 'inside-F'],
+        'J' => [0, 'leaving'],
+        '7' => [0, 'entering'],
+    },
+);
+
+my $area = 0;
+
+while (my ($y, $row) = each @cycle_grid) {
+    my $status = 'outside';
+    while (my ($x, $char) = each $row->@*) {
+        my $next = $algorithm{$status}{$char};
+        if (! defined $next) {
+            warn "Undefined: $status, $x, $y, $char\n";
+        } elsif ($next->[0]) {
+            #say "count ($y, $x) '$char': $status => $next->[1]";
+        }
+        $area += $next->[0];
+        $status = $next->[1];
+    }
+}
+
+say $area;
